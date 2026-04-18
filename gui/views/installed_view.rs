74f163df274614767fd05bd29b0b4d5205779347
Omega <emomega@proton.me>
2026-04-18 16:29:36 -0400

    Create installed_view.rs

---
+++ gui/views/installed_view.rs
@@ -0,0 +1,14 @@
+pub struct InstalledView;
+
+impl InstalledView {
+    pub fn render() {
+        println!("📦 Installed Packages");
+
+        println!("(placeholder data)");
+
+        println!("  - vim");
+        println!("  - git");
+        println!("  - curl");
+        println!("  - flatpak");
+    }
+}
