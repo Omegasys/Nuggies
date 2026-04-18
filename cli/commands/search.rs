f8cbca6c4a97bd10bd7a9f8677991bf3a757957b
Omega <emomega@proton.me>
2026-04-18 15:22:30 -0400

    Create search.rs

---
+++ cli/commands/search.rs
@@ -0,0 +1,6 @@
+pub fn run(query: &str) {
+    println!("[nuggies] Searching for: {}", query);
+
+    // Placeholder (future: call pkg layer)
+    println!("-> No results (backend not implemented yet)");
+}
