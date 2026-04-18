c496dbd8bf054d687a6dc1da4410296df68eed0c
Omega <emomega@proton.me>
2026-04-18 15:23:17 -0400

    Create compare.rs

---
+++ cli/commands/compare.rs
@@ -0,0 +1,7 @@
+pub fn run(package: &str) {
+    println!("[nuggies] Comparing package formats for: {}", package);
+
+    println!("-> flatpak: unknown");
+    println!("-> native: unknown");
+    println!("-> appimage: unknown");
+}
