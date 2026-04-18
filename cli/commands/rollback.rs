fcc63917866c76fef4bd789d0b7a72de2c4ac915
Omega <emomega@proton.me>
2026-04-18 15:23:08 -0400

    Create rollback.rs

---
+++ cli/commands/rollback.rs
@@ -0,0 +1,8 @@
+pub fn run(package: &str, version: &str) {
+    println!(
+        "[nuggies] Rolling back {} to version {}",
+        package, version
+    );
+
+    println!("-> (backend not implemented)");
+}
